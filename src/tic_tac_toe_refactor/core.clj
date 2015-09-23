cac82b661748d32a6d741b53df5452e683727585
Mary Baylis <mary.c.baylis@gmail.com>
2015-09-22 21:34:22 -0400

    Allow user to exit at any prompt.

--- src/tic_tac_toe_refactor/core.clj
+++ src/tic_tac_toe_refactor/core.clj
@@ -1,7 +1,27 @@
 (ns tic-tac-toe-refactor.core
-  (:gen-class))
+  (:gen-class)
+  (:require [tic-tac-toe-refactor.data :as data]))
+
+(defn get-input
+  []
+  (def input (str (read-line)))
+  (if (some #{input} data/quit-list)
+    (System/exit 0)
+    input))
+
+(defn play-again?
+  [input]
+  false
+)
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "I play tic-tac-toe!"
   [& args]
-  (println "Hello, World!"))
+  (println "Welcome to Tic-Tac-Toe!")
+  (println "Shall we play a game?  (Y)es/(N)o?")
+
+  (if (play-again? (get-input))
+      (println "Play a game!")
+      (println "Why'd you even?....."))
+  (println "See you next time!")
+)
